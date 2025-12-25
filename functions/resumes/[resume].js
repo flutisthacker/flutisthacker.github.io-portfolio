f19b9765c326388284cb1cffde83bbe36895a2ed
flutisthacker <saman.maharjan96@gmail.com>
2025-12-25 20:29:14 +0545

    test

--- functions/resumes/[resume].js
+++ functions/resumes/[resume].js
@@ -1,10 +1,10 @@
 export function onRequest(context) {
   return new Response(JSON.stringify(getMetaData(context.params.resume)));
 }
-
+let myData;
 export function getMetaData(x){
     fetchJSONData("../../saman.json"); 
-    return {mydata:{name:"saman"}}
+    return {mydata:{name:"saman",data:myData}}
 }
 
 function fetchJSONData(url) {
@@ -15,7 +15,9 @@ function fetchJSONData(url) {
                     }
                     return response.json();  
                 })
-                .then(data => console.log(data))  
+                .then(data => {
+                    myData=data
+                })  
                 .catch(error => console.error('Failed to fetch data:', error)); 
         }
         
