c2bda4a285afd44cf17c6c41d0bd35e5fa966701
flutisthacker <saman.maharjan96@gmail.com>
2025-12-25 20:27:44 +0545

    test

--- functions/resumes/[resume].js
+++ functions/resumes/[resume].js
@@ -3,5 +3,19 @@ export function onRequest(context) {
 }
 
 export function getMetaData(x){
+    fetchJSONData("../../saman.json"); 
     return {mydata:{name:"saman"}}
 }
+
+function fetchJSONData(url) {
+            fetch(url)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error(`HTTP error! Status: ${response.status}`);
+                    }
+                    return response.json();  
+                })
+                .then(data => console.log(data))  
+                .catch(error => console.error('Failed to fetch data:', error)); 
+        }
+        
