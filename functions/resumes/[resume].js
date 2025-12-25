325c93e686deca9e8af578ee3d66a427de0b6a1a
flutisthacker <saman.maharjan96@gmail.com>
2025-12-25 22:39:49 +0545

    update

--- functions/resumes/[resume].js
+++ functions/resumes/[resume].js
@@ -208,6 +208,128 @@ let myData2={
                 "linkedin":"https://www.linkedin.com/in/saman-maharjan-97s/"
             }
         },
+        "aarju":{
+    "personal":{
+        "title":"Aarju Dangol",
+        "contact":"",
+        "email":"aarjudangol10@gmail.com",
+        "location":"Sanepa, Kathmandu",
+        "position":""
+    },
+    "awards":[
+        {
+            "title":"Dean Award",
+            "location":"Taylors University",
+            "date":"2022"
+        }
+    ],
+    "skills":["Initiative",
+        "EagertoLearn",
+        "Workingin/as a team",
+        "Adaptability",
+        "Customer Insight",
+"Customer Research",
+"Customer Experience"
+    ],
+    "language":["English","Newari","Nepali","Hindi"],
+    "experience":[
+        {
+            "title":"Accountant",
+            "date":"2020-2025",
+            "location":"Stallion",
+            "logo":"kudo.png",
+            "description":"Assisted in designing wireframes and interactive prototypes using Figma for a mobile-based e-health application.;Created user personas and journey maps to build user-focused designs.;Developed responsive web page layouts and mobile app interfaces based on UI/UX best practices.; Developed low-fidelity and high-fidelity prototypes to simulate user interactions.  ",
+            "points":"Analyze and clarify requirements with team members.||Performed manual functional testing of application features and bugs.||Writing test cases and scenarios for the new features and bugs using test rails.||Performed various types of testing such as sanity, smoke, and regression testing in non-production and production environments.||Usedtestrails for production sanity testing and ensuring production stability.||WorkedwithTrello and Jira for project management.||Collaborated with DevOps and Developers for testing new application features and infrastructure changes.||WorkedwithKanban and sprint methodologies in a team.||WorkedinAPItesting on features with the help of Postman||WorkedwithAzure SDK and websockets for machine translation for human speech||Partly handled scrum on project and Release management||Testcase automation with test agent.||Datavalidation to test AI with LLM model implementation.||Extendedtest on integration and POC applications||PlannedandReviewed test plan and scenario||Verification of Application vulnerability threats||Co-Organized onsite event for the team.|| Performed manual functional testing of the application features.|| Identify problem areas and bugs of the application.|| Re-test fixed defects.|| Ensureproduction environment sanity testing and stabilization.|| Performed various types of testing such as sanity, smoke, and regression testing.|| Workedinateamanddiscussed bugs with developers with limited access.|| Exploratory testing on AI-based application|| Functional testing for the data-based applications.|| Usedgoogledocs for documenting bug lists.|| UsedTrello for bug tracking.|| LearnedAPI testing for AI based applications."
+        },
+        {
+            "title":"Administrative Office Assistant",
+            "date":"November 2023 - May 2025 (1 year 7 months)",
+            "location":"Pratham Trade Line Pvt ltd",
+            "logo":"kudo.png",
+            "description":"Managed billing operations in the FMCG sector, handled letter of credit (LC) documentation, processed cheque deposits, and coordinated with sales associates to ensure smooth financial and sales operations.",
+            "points":"Developed application features for e-commerce platforms.|| Fixed application bugs and developed features according to clients’ needs.|| Developed dashboard and e-commerce websites using API application and liquid templating.|| UsedTrello, Gitlab/Github/Bitbucket for project management.|| Workedwithversion control systems.|| LearnedRuby onRails and liquid templating.|| Fixedandtested application bugs.|| Learnedsoftware development methodologies."
+        },
+        {
+            "title":"Administrative Assistant",
+            "date":"November 2022 - October 2023 (1 year)",
+            "location":"Zoom dental clinic Pvt ltd",
+            "logo":"kudo.png",
+            "description":"Managed billing operations in the FMCG sector, handled letter of credit (LC) documentation, processed cheque deposits, and coordinated with sales associates to ensure smooth financial and sales operations.",
+            "points":"Developed application features for e-commerce platforms.|| Fixed application bugs and developed features according to clients’ needs.|| Developed dashboard and e-commerce websites using API application and liquid templating.|| UsedTrello, Gitlab/Github/Bitbucket for project management.|| Workedwithversion control systems.|| LearnedRuby onRails and liquid templating.|| Fixedandtested application bugs.|| Learnedsoftware development methodologies."
+        },
+        {
+            "title":"Front Desk Attendant",
+            "date":"June 2021 - July 2022 (1 year 2 months)",
+            "location":"Zoom dental clinic Pvt ltd",
+            "logo":"kudo.png",
+            "description":"Managed billing operations in the FMCG sector, handled letter of credit (LC) documentation, processed cheque deposits, and coordinated with sales associates to ensure smooth financial and sales operations.",
+            "points":"Developed application features for e-commerce platforms.|| Fixed application bugs and developed features according to clients’ needs.|| Developed dashboard and e-commerce websites using API application and liquid templating.|| UsedTrello, Gitlab/Github/Bitbucket for project management.|| Workedwithversion control systems.|| LearnedRuby onRails and liquid templating.|| Fixedandtested application bugs.|| Learnedsoftware development methodologies."
+        }
+    ],
+    "training":[
+        {
+            "title":"Scrum Master workshop",
+            "date":"2022",
+            "location":"Prakash Aryal",
+            "description":"Registered Scrum Master under the Scrumlab platform. Short workshop on learning the scrum methods and its implementation."
+        }
+    ],
+    "projects involved":[],
+    "education":[
+        {
+            "title":"Masters in Business",
+            "name":"Oxbridge International HSS, Hattisar",
+            "date":"2025",
+            "description":"I am currently pursuing a Master’s degree in Computer Science, focusing on advanced topics such as algorithms, machine learning, and software engineering. This program is enhancing my expertise in developing innovative solutions and tackling complex computational problems. My studies are deepening my understanding of cutting-edge technologies and equipping me with the skills needed to drive impactful projects in the tech industry.",
+            "logo":"lincoln.png",
+            "marks":"",
+            "location":"Putalisadak, Kathmandu"
+        },
+        {
+            "title":"Bachelor of Business Studies, Business, Management, Marketing, and Related Support Service",
+            "name":"Campion College ",
+            "date":"2025",
+            "description":"I hold a Bachelor's degree with Honors in Computing Science, where I gained a solid foundation in core computing principles and technologies. My coursework covered key areas such as programming, data structures, algorithms, and software development, equipping me with both theoretical insights and practical skills. This degree provided me with a robust understanding of computing fundamentals and problem-solving techniques, preparing me for diverse roles in the technology sector.",
+            "logo":"IIMS.png",
+            "marks":"",
+            "location":"Putalisadak, Kathmandu"
+        },
+        {
+            "title":"High School",
+            "name":"Oxbridge International HSS",
+            "date":"2020",
+            "description":"I have pursued higher education in Computer Science, building on a foundational background in Commerce. This unique combination has allowed me to integrate technical proficiency with a strong understanding of business principles. My studies in Computer Science have provided me with expertise in programming, data analysis, and system design, while my commerce background has equipped me with insights into financial management, market analysis, and strategic planning. This interdisciplinary approach enables me to effectively bridge the gap between technology and business, driving innovative solutions that align with organizational goals.",
+            "logo":"eic.png",
+            "marks":"3.26 GPA",
+            "location":"Sanepa, Kathmandu"
+        },
+        {
+            "title":"Radiant Reader's Academy",
+            "name":"Radiant Reader's Academy",
+            "date":"2017-2018",
+            "description":"I completed my secondary education with excellent results, demonstrating strong academic performance across a broad range of subjects. This solid foundation in key areas such as mathematics, science, and language arts has equipped me with essential skills in problem-solving, critical thinking, and effective communication. My commitment to academic excellence during this period laid the groundwork for my continued success in higher education and professional endeavors.",
+            "logo":"IIMS.png",
+            "marks":"3.45 GPA",
+            "location":"Kalimati, Kathmandu"
+        }
+    ],
+    "summary":"",
+    "references":[
+        {
+            "name":"Anil Shrestha",
+            "company":"KUDO.inc",
+            "title":"QA Automation Engineer",
+            "email":"nl.shrestha@gmail.com",
+            "phone":""
+        }
+    ],
+    "links":{
+        "facebook":"https://www.facebook.com/aarjudangol10",
+        "x":"https://www.twitter.com/maharjandsaman",
+        "instagram":"https://www.instagram.com/aarjudangol321",
+        "linkedin":"https://www.linkedin.com/in/aarju-dangol-7115a1246"
+    }
+},
         "delisha":{
     "personal":{
         "name":"Delisha Maharjan",
