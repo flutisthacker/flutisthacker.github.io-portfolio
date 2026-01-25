654500ce6ddb53afc069a1870ef90c8e6db19545
flutisthacker <saman.maharjan96@gmail.com>
2026-01-25 14:00:15 +0545

    test2

--- functions/ids/[id].js
+++ functions/ids/[id].js
@@ -6,7 +6,11 @@ panID: 231
 let toSend={}
 export function onRequest(context) {
     try{
-        toSend[context.params.id]=me[context.params.id]
+        if (context.params.id=="all"){
+            toSend=me
+        }else{
+            toSend[context.params.id]=me[context.params.id]
+        }
         return new Response(JSON.stringify(toSend));
     }catch(e){
         return new Response(JSON.stringify({"status":404}));        
